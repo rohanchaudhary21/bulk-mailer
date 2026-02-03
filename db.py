c41775b06854cc663ad3f10cf0635a5c6602f1c3
Rohan Chaudhary <rohanchaudhary2105@gmail.com>
2026-02-03 20:01:36 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,27 +1,29 @@
 import sqlite3
 
+DB_NAME = "stats.db"
+
 def get_db():
-    conn = sqlite3.connect("app.db", check_same_thread=False)
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
     db = get_db()
-    cursor = db.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS oauth_tokens (
-        user_email TEXT PRIMARY KEY,
-        token_json TEXT
-    )
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS email_logs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT,
+            status TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
     """)
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS email_logs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT,
-        status TEXT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS oauth_tokens (
+            user_email TEXT PRIMARY KEY,
+            token_json TEXT NOT NULL
+        )
     """)
 
     db.commit()
